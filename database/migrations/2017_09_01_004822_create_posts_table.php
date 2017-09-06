85e942edb4d618e625a8755e95be8f3177eae849
Ashraf Quadri Syed <emails2ashraf@gmail.com>
2017-09-06 10:24:12 -0700

    littel cahnges

--- database/migrations/2017_09_01_004822_create_posts_table.php
+++ database/migrations/2017_09_01_004822_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
             Schema::create('posts', function(Blueprint $table) {
                 $table->increments('id');
                 $table->string('title');
-$table->text('body');
+                $table->text('body');
 
                 $table->timestamps();
                 $table->softDeletes();
